7a22cf0e0e6d6f688fcc62cdf4eac3c80b234f34
Ryan Moakes <m015485n@student.staffs.ac.uk>
2026-01-08 19:12:00 +0000

    Laptop Fix

--- m015485n_Project/FrameworkDX11/GameObject.cpp
+++ m015485n_Project/FrameworkDX11/GameObject.cpp
@@ -79,7 +79,7 @@ void GameObject::CreateSampler(ID3D11Device* m_pd3dDevice, ID3D11DeviceContext* 
 {
 	D3D11_SAMPLER_DESC sampDesc;
 	ZeroMemory(&sampDesc, sizeof(sampDesc));
-	sampDesc.Filter = D3D11_FILTER_MAXIMUM_MIN_MAG_MIP_POINT;
+	sampDesc.Filter = D3D11_FILTER_MIN_MAG_MIP_POINT;
 	sampDesc.AddressU = D3D11_TEXTURE_ADDRESS_WRAP;
 	sampDesc.AddressV = D3D11_TEXTURE_ADDRESS_WRAP;
 	sampDesc.AddressW = D3D11_TEXTURE_ADDRESS_WRAP;
